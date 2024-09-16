6886de0a863bae060a25f01993f98febc25547b2
Sudipta <sudipta.42396@gmail.com>
2024-09-17 01:56:07 +0600

    Cart updates

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import RegistrationForm from "../components/forms/RegistrationForm";
+import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
     <div>
       <RegistrationForm />
+      <Link to="/login">Click Here to Login</Link>
     </div>
   );
 }
