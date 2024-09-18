0e9147dc167f45c6b2bafbfb45743573acb3d66f
evan-jaber <evan.masrur.jaber@g.bracu.ac.bd>
2024-09-18 19:33:32 +0600

    Product delete module added

---
+++ api/routes/delProdRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import db from "../db.js";
+
+const router = express.Router();
+
+router.post("/delProd", (req, res) => {
+    const prodID = req.body;
+    const delsql = "DELETE FROM Product WHERE product_id = ?";
+    db.query(delsql, [prodID], (err, result) => {
+        if (err) {
+            return res.json({ success: false, message: err.message });
+        }
+    });
+});
+export default router;
