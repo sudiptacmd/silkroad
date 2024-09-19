c57d839d8c75986a9010673a15bd974345283c68
Sudipta <sudipta.42396@gmail.com>
2024-09-20 01:34:34 +0600

    Reviews

--- client/src/components/ui/Reviews.jsx
+++ client/src/components/ui/Reviews.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Reviews(props) {
   const { reviewData } = props;
   return (
-    <div>
-      <p>
-        {reviewData.firstName} {reviewData.lastName}
-      </p>
+    <div className="p-4 bg-[#f1f0f0]">
+      <div className="flex flex-row gap-4">
+        <p className="font-semibold">
+          {reviewData.firstName} {reviewData.lastName}
+        </p>
+        <p>{reviewData.rating} stars</p>
+      </div>
+      <div className="bg-[#c5c1c1c2] p-2">{reviewData.content}</div>
     </div>
   );
 }
